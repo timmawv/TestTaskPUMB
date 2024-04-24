4d14406c91ed9e3a43501105eb2d2c36c8b70b58
Timur <91984444+timcol1@users.noreply.github.com>
2024-04-24 19:02:07 +0300

    Initial commit

---
+++ src/main/java/avlyakulov/timur/TestTaskPUMB/TestTaskPumbApplication.java
@@ -0,0 +1,13 @@
+package avlyakulov.timur.TestTaskPUMB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskPumbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskPumbApplication.class, args);
+	}
+
+}
