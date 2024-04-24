2853466f419700d31514e5cfa1674607abc8239b
Timur <91984444+timcol1@users.noreply.github.com>
2024-04-24 21:08:26 +0300

    add logger to controller

--- src/main/java/avlyakulov/timur/TestTaskPUMB/controller/FileController.java
+++ src/main/java/avlyakulov/timur/TestTaskPUMB/controller/FileController.java
@@ -1,6 +1,8 @@
 package avlyakulov.timur.TestTaskPUMB.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/files")
 public class FileController {
-
+    Logger logger = LoggerFactory.getLogger(FileController.class);
     @GetMapping("/uploads")
     public ResponseEntity<?> getFiles() {
         return ResponseEntity.ok("Hello");
